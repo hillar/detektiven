826b7876db24f61245f3a2fbcf3c7660c234838d
Hillar Aarelaid <hillar@aarelaid.net>
2017-06-15 00:22:59 +0300

    some basics issuer and subject test + cli params

--- chat/server/test.js
+++ chat/server/test.js
@@ -1,14 +1,14 @@
 'use strict'
-
+const cliParams = require('commander');
 const wsocket = require('ws')
 const crypto = require('crypto')
 const fs = require('fs')
 const jwt = require('jsonwebtoken')
 
-console.log('Server started')
+// helper functions
+// TODO move to separate fail
 
-var wss = new wsocket.Server({port: 3000})
-
+// pem'ify cert, so x509 and jwt like it more
 function cert (token) {
   const parsed = jwt.decode(token, {complete: true, json: true})
   if (parsed && parsed.header.x5c) { return '-----BEGIN CERTIFICATE-----\n' + parsed.header.x5c[0] + '\n-----END CERTIFICATE-----' } else { return '' }
@@ -23,6 +23,40 @@ function getNonce (l) {
   for (var i = 0; i < l; i++) val += hex.charAt(Math.floor(Math.random() * hex.length))
   return val
 }
+
+// main
+cliParams
+  .version('0.0.1')
+  .usage('[options] <file ...>')
+  .option('-i, --issuers [file]', 'issuers list','./issuer.commonNames')
+  .option('-s, --subjects [file]','subjects list','./subject.serialNumbers')
+  .option('-p, --port [number]','port to listen',3000)
+  .parse(process.argv);
+//console.log(' args: %j', cliParams.args);
+console.log('Starting server with:', cliParams.issuers, cliParams.subjects, cliParams.port);
+// load issuers and subjects
+try {
+var issuers = JSON.parse(fs.readFileSync(cliParams.issuers,'utf8'));
+} catch(e) {
+  console.error('failed to load issuers',e);
+  process.exit(1);
+}
+if (!issuers.names || issuers.names.length < 1) {
+  console.error('no issuers');
+  process.exit(1);
+}
+try {
+  var subjects = JSON.parse(fs.readFileSync(cliParams.subjects,'utf8'));
+} catch(e) {
+  console.error('failed to load subjects',e);
+  process.exit(1);
+}
+if (! subjects.serials || subjects.serials.length < 1) {
+  console.error('no subjects');
+  process.exit(1);
+}
+// create websocketserver
+var wss = new wsocket.Server({port: cliParams.port})
 
 // On every new connection
 wss.on('connection', function (ws) {
@@ -40,18 +74,27 @@ wss.on('connection', function (ws) {
       ws.terminate();
     } else {
       // we good to check the token
-      const sha = crypto.createHash('sha1').update(JSON.stringify(message.token)).digest('hex')
-      jwt.verify(message.token, cert(message.token), {ignoreExpiration: true, nonce: nonce}, (err, decoded) => {
-        if (err) {
-          ws.send('No, no, no ...')
-          ws.terminate()
-          console.log('Did not verify', err)
-        } else {
-          fs.writeFileSync('./like.' + sha, JSON.stringify(message.token))
-          ws.send('We like you too, ' + decoded.sub)
-          console.log('Verified', decoded.sub)
-        }
-      })
+      //const sha = crypto.createHash('sha1').update(JSON.stringify(message.token)).digest('hex')
+      var pubcrt = cert(message.token);
+      var crt = x509.parseCert(pubcrt);
+      var issuerOK = issuers.names.includes(crt.issuer.commonName);
+      var subjectOK = subjects.serials.includes(crt.subject.serialNumber);
+      if ( issuerOK && subjectOK ) {
+        jwt.verify(message.token, pubcrt, {ignoreExpiration: true, nonce: nonce}, (err, decoded) => {
+          if (err) {
+            ws.send('No, no, no ...')
+            ws.terminate()
+            console.log('Did not verify', err)
+          } else {
+            //fs.writeFileSync('./like.' + sha, JSON.stringify(message.token))
+            ws.send('We like you too, ' + decoded.sub)
+            console.log('Verified', decoded.sub)
+          }
+        });
+      } else {
+        ws.terminate();
+        console.log('issuer:', issuerOK, 'subject:', subjectOK);
+      }
     }
   })
 })
