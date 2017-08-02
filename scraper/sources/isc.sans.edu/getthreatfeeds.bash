5cc21c8eca260a19b01e234a0b2e342c8cfad015
Hillar <hillar@users.noreply.github.com>
2017-08-02 09:44:44 +0300

    rm "no updates file"

--- scraper/sources/isc.sans.edu/getthreatfeeds.bash
+++ scraper/sources/isc.sans.edu/getthreatfeeds.bash
@@ -3,6 +3,10 @@ while true; do
   TIME=$(date +%s)
   curl -s https://isc.sans.edu/api/threatfeeds/?json | jq -r '(map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $cols, $rows[] | @csv'| gzip -9 > threatfeeds.$TIME.csv.gz
   echo "$(zcat threatfeeds.$TIME.csv.gz | cut -f4 -d,| sort| uniq| tail -2 | head -1)" >> lastupdates.log
-  [ $(tail -2 lastupdates.log | sort|uniq| wc -l) -eq 1 ] || ./getfeeds.bash threatfeeds.$TIME.csv.gz
+  if [ ! $(tail -2 lastupdates.log | sort|uniq| wc -l) -eq 1 ]; then
+    ./getthreatlistX.bash threatfeeds.$TIME.csv.gz
+  else
+    rm threatfeeds.$TIME.csv.gz
+  fi
   sleep 61
 done
--- scraper/sources/isc.sans.edu/pushnews.bash
+++ scraper/sources/isc.sans.edu/pushnews.bash
@@ -2,11 +2,13 @@ echo "$1"
 name="$1"
 TIME=$(date +%s)
 cd $name || exit
+APIURL="https://localhost/api/v0/first_last_ip"
+BEARER="xxxxx.yyyy.zzz"
 ll=$(ls *.csv.gz | tail -2)
 if [ $(echo "$ll" | wc -l) -eq 2 ]; then
-  zdiff $ll | grep ">"| sed 's/> //'| sed 's/""//g' | jq -c -j -R -r 'split("\n") | map(split(",")) | map({"firstseen":.[0],"lastseen":.[2],"ip":.[1]})'> $name.$TIME.news
+  zdiff $ll | grep ">"| sed 's/> //'| sed 's/"//g' | jq -c -j -R -r 'split("\n") | map(split(",")) | map({"firstseen":.[0],"lastseen":.[2],"ip":.[1]})'> $name.$TIME.news
   [ $(cat $name.$TIME.news | wc -l) -gt 1 ] || exit
   cat $name.meta.json >> $name.$TIME.news
-    echo "curl -XPOST --compressed -H "Authorization: Token xxxxxxxxxxxxxx" -H "Content-Type: application/x-ndjson" http://localhost:1234/api/v0/first_ip_last --data @$name.$TIME.news"
+  echo "curl -XPOST --compressed -H "Authorization: Bearer $BEARER" -H "Content-Type: application/x-ndjson" $APIURL --data @$name.$TIME.news"
 fi
 cd ..
