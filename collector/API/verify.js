5ed5180b718375a59838c340bf33fe013b54b047
Hillar <hillar@users.noreply.github.com>
2017-08-07 16:03:50 +0300

    verify JWT token with nodejs

---
+++ collector/API/verify.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+const jwt = require('jsonwebtoken');
+const cliParams = require('commander');
+
+cliParams
+  .version('0.0.1')
+  .option('-t, --token <file>', 'token')
+  .option('-p, --publickey <file>', 'public key','./publickey.pem')
+  .parse(process.argv);
+
+var publickey = fs.readFileSync(cliParams.publickey);
+var token = fs.readFileSync(cliParams.token).toString().replace(/[\n\r]/g, '');
+
+jwt.verify(token, publickey, { algorithms: ['RS256']},function(err, decoded){
+  if (err){
+    console.error(err);
+
+  } else {
+    console.log(JSON.stringify(decoded));
+  }
+});
