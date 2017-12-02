58fbde711e5bd4e30c7d1f8a40d214910d7fdaf4
Hillar Aarelaid <hillar@aarelaid.net>
2017-12-02 14:47:01 +0200

    pumped OSSVERSION

--- stuff/txt/opensemanticsearch/installOpenSemanticSearch.bash
+++ stuff/txt/opensemanticsearch/installOpenSemanticSearch.bash
@@ -16,7 +16,7 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 #https://www.opensemanticsearch.org/download/open-semantic-search_17.11.17.deb
-OSSVERSION="17.11.17"
+OSSVERSION="17.12.01"
 
 echo "$(date) preparing system .."
 echo 'Acquire::ForceIPv4 "true";' | sudo tee /etc/apt/apt.conf.d/99force-ipv4
@@ -27,11 +27,13 @@ apt-get -y upgrade >> /vagrant/provision.log 2>&1
 cd /vagrant/
 [ -f open-semantic-search_$OSSVERSION.deb ] || wget -q https://www.opensemanticsearch.org/download/open-semantic-search_$OSSVERSION.deb
 echo "$(date) installing depes .."
+# see https://raw.githubusercontent.com/opensemanticsearch/open-semantic-search/master/build/deb/stable/DEBIAN/control
+apt-get -y install tesseract-ocr scantailor
 apt-get -y install default-jre-headless apache2 libapache2-mod-php php php-bcmath libapache2-mod-wsgi-py3 python3-django python3-rdflib python3-pysolr python3-dateutil python3-lxml python3-feedparser poppler-util pst-utils daemon python3-pyinotify python3-celery python3-nltk >> /vagrant/provision.log 2>&1
 echo "$(date) installing open-semantic-search .."
 # TODO fix install hunging here ;(
-#dpkg -i open-semantic-search_$OSSVERSION.deb
-#apt-get -y -f install
+dpkg -i open-semantic-search_$OSSVERSION.deb
+apt-get -y -f install
 #sleep 2
 #echo "$(date) indexing sample documents .."
 #opensemanticsearch-index-dir /vagrant/sampledocs/
