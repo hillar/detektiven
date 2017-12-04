815d7ccbefc14719e5942af26ec8f9f9c088d8b3
Hillar Aarelaid <hillar@aarelaid.net>
2017-12-04 22:44:29 +0200

    user data

--- stuff/txt/opensemanticsearch/oss-mini/server.js
+++ stuff/txt/opensemanticsearch/oss-mini/server.js
@@ -5,22 +5,35 @@ const url = require('url');
 const fs = require('fs');
 const path = require('path');
 
-let portTarget = 8983
-let portListen = 9983
+let portTarget = 8983 // solr
+let portListen = 9983 
 let ipListen = '192.168.11.2'
 let staticDir = '../solr-buefy/test1/dist'
 // authentication
+let users = {}
 let basic = auth.basic({
 		realm: "oss-mini"
 	}, (username, password, callback) => { 
+    //if (users[username]) return true
 		callback(username === "kala" && password === "maja");
 	}
 );
 basic.on('success', (result, req) => {
-	console.log(`User authenticated: ${result.user}`);
+  if (!users[result.user]){
+    users[result.user] = {logintime:Date.now(), lastseen:Date.now(), ip: req.socket.remoteAddress}
+  } else {
+    users[result.user]['lastseen'] = Date.now()
+    if (req.socket.remoteAddress != users[result.user]['ip']) {
+      console.log('WARNING! user', req.user, 'has new ip',req.socket.remoteAddress,'old',users[req.user]['ip'])
+      //TODO send email to user
+    }
+  }
+  let user_online = users[result.user]['lastseen'] - users[result.user]['logintime']
+	console.log(`User ${result.user} authenticated since ${users[result.user]['logintime']} online time ${user_online}`);
 });
 
 basic.on('fail', (result, req) => {
+  delete users[result.user]
 	console.log(`User authentication failed: ${result.user}`);
 });
 
@@ -31,7 +44,7 @@ basic.on('error', (error, req) => {
 // api is prxy for solr
 let proxy = httpProxy.createProxyServer({});
 proxy.on('proxyReq', function(proxyReq, req, res, options) {
-  console.log('proxy',Date.now(),req.socket.remoteAddress,req.user,req.url,JSON.stringify(req.headers))
+  console.log('proxy',Date.now(),req.socket.remoteAddress,req.user,req.url,JSON.stringify(req.headers['user-agent']))
 });
 proxy.on('error', function (err, req, res) {
   console.error('proxy ERROR',Date.now(),err)
@@ -41,59 +54,82 @@ proxy.on('error', function (err, req, res) {
   res.end('Something went wrong.');
 });
 
-
+// server
 let server = http.createServer(basic, (req, res) => {
+  // solr queries proxied 
   if (req.url.startsWith("/solr/core")) {
-    proxy.web(req, res, {
-        target: `http://127.0.0.1:${portTarget}`
-    });
+    if (users[req.user]['fp']) {
+      proxy.web(req, res, {
+          target: `http://127.0.0.1:${portTarget}`
+      });
+    }  
   } else {
     console.log('query',Date.now(),req.socket.remoteAddress,req.user,req.url,JSON.stringify(req.headers))
-    if (req.url.startsWith("/")) {
-      const parsedUrl = url.parse(req.url)
-      let pathname = `${staticDir}${parsedUrl.pathname}`;
-      console.log('static:',pathname,';')
-      const mimeType = {
-                      '.ico': 'image/x-icon',
-                      '.html': 'text/html',
-                      '.js': 'text/javascript',
-                      '.json': 'application/json',
-                      '.css': 'text/css',
-                      '.png': 'image/png'}
-      fs.exists(pathname, function (exist) {
-        if(!exist) {
-          res.statusCode = 404
-          res.end(`File ${pathname} not found!`)
-          return
-        }
-        if (fs.statSync(pathname).isDirectory()) {
-          res.writeHead(302, {
-                "Location": `${parsedUrl.pathname}index.html`
-          })
-          res.end()
-          return
-        }
-        // read file from file system
-        fs.readFile(pathname, function(err, data){
-          if(err){
-            res.statusCode = 500
-            res.end(`Error getting the file: ${err}.`)
-          } else {
-            // based on the URL path, extract the file extention. e.g. .js, .doc, ...
-            const ext = path.parse(pathname).ext
-            // if the file is found, set Content-type and send data
-            res.setHeader('Content-type', mimeType[ext] || 'text/plain' )
-            res.end(data)
+    // get to / with params .. obscurity
+    if (req.url.startsWith("/?")) {
+        const parsedUrl = url.parse(req.url)
+        let bits = parsedUrl.query.split('&')
+        let params = []
+        for ( let bit of bits ) {
+          [key,value] = bit.split('=')
+          params.push(key)
+          if (key == 'fp' || key == 'pk') {
+              if (users[req.user][key]) {
+                if (users[req.user][key] != value) {
+                  console.log('WARNING! user', req.user, 'has new',key,value,'old',users[req.user][key])
+                  //TODO send email to user
+                }
+              } else {
+                users[req.user][key] = value
+            }
           }
-        })
-      })
-      
-      
+        }
+        res.end()
+        console.log('got user',req.user,'new params',params.join(','),'all',JSON.stringify(users[req.user]))
     } else {
-        res.writeHead(302, {
-              "Location": "/index.html"
-        });
-        res.end(); 
+      // static files
+      if (req.url.startsWith("/")) {
+        const parsedUrl = url.parse(req.url)
+        let pathname = `${staticDir}${parsedUrl.pathname}`;
+        console.log('static:',pathname,';')
+        const mimeType = {
+                        '.ico': 'image/x-icon',
+                        '.html': 'text/html',
+                        '.js': 'text/javascript',
+                        '.json': 'application/json',
+                        '.css': 'text/css',
+                        '.png': 'image/png'}
+        fs.exists(pathname, function (exist) {
+          if(!exist) {
+            res.statusCode = 404
+            res.end(`File ${pathname} not found!`)
+            return
+          }
+          if (fs.statSync(pathname).isDirectory()) {
+            res.writeHead(302, {
+                  "Location": `${parsedUrl.pathname}index.html`
+            })
+            res.end()
+            return
+          }
+          // read file from file system
+          fs.readFile(pathname, function(err, data){
+            if(err){
+              res.statusCode = 500
+              res.end(`Error getting the file: ${err}.`)
+            } else {
+              const ext = path.parse(pathname).ext
+              res.setHeader('Content-type', mimeType[ext] || 'text/plain' )
+              res.end(data)
+            }
+          })
+        })
+      } else {
+          res.writeHead(302, {
+                "Location": "/index.html"
+          });
+          res.end(); 
+      }
     }
   }
 })
