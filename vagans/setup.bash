2a9ca89e3290150ede7693107248dc0308ebb9c5
HILLAR AARELAID <hillar.aarelaid@politsei.ee>
2018-04-02 15:46:59 +0300

    added clamav

--- vagans/setup.bash
+++ vagans/setup.bash
@@ -147,6 +147,8 @@ ipa80=$(curl -s ${ipa_ip} | wc -l)
 if [ $ipa80 -ne 375 ]; then
    log "WARNING IPA ${IPA} ${ipa_ip} did not replied as expected"
 else
+  ldapsearch -x -D "uid=demo,cn=users,cn=accounts,dc=example,dc=org" -w password -h ${ipa_ip} -b "cn=accounts,dc=example,dc=org" -s sub 'uid=demo'
+  echo $?
   log "IPA ${IPA} ${ipa_ip} seems ok"
 fi
 
@@ -176,7 +178,35 @@ tika69=$(curl -s ${tika_ip}:9998 | wc -l)
 if [ $tika69 -ne 69 ]; then
    log "WARNING TIKA ${TIKA} ${tika_ip} did not replied as expected"
 else
-  ldapsearch -x -D "uid=demo,cn=users,cn=accounts,dc=example,dc=org" -w password -h ${ipa_ip} -b "cn=accounts,dc=example,dc=org" -s sub 'uid=demo'
-  echo $?
   log "TIKA ${TIKA} ${tika_ip} seems ok"
+fi
+
+CLAMAV='clamav'
+if [ ! $(vm_exists ${CLAMAV}) = '0' ]; then
+  log "creating CLAMAV ${CLAMAV}"
+  virt-clone -q  -o ${FB} -n ${CLAMAV} --auto-clone
+  start_vm ${CLAMAV} >> $DEBUGLOG 2>&1
+  clamav_ip=$(getip_vm ${CLAMAV})
+  [ $? -ne 0 ] && die "failed to get ip address for vm ${CLAMAV}"
+  ssh-keygen -f "/root/.ssh/known_hosts" -R ${clamav_ip} >> $DEBUGLOG 2>&1
+  ssh -oStrictHostKeyChecking=no -i ${USERNAME}.key ${USERNAME}@${ipa_ip} "sudo su -c 'echo ${CLAMAV}.${DOMAIN} > /etc/hostname'; sudo hostname ${CLAMAV}.${DOMAIN}" >> $DEBUGLOG 2>&1
+  [ -f $BOXESDIR/scripts/install-clamav.bash ] || wget --no-check-certificate -q https://raw.githubusercontent.com/hillar/detektiven/master/vagans/install-clamav.bash -O $BOXESDIR/scripts/install-clamav.bash
+  [ $? -ne 0 ] && die "${CLAMAV} failed to download install-clamav.bash" 
+  [ -f $BOXESDIR/scripts/install-clamav.bash ] || die "${CLAMAV} missing install-clamav.bash"
+  scp -oStrictHostKeyChecking=no -i ${USERNAME}.key $BOXESDIR/scripts/install-clamav.bash ${USERNAME}@${clamav_ip}: >> $DEBUGLOG 2>&1
+  ssh -oStrictHostKeyChecking=no -i ${USERNAME}.key ${USERNAME}@${clamav_ip} "sudo bash -x /home/${USERNAME}/install-clamav.bash ${clamav_ip}" >> $DEBUGLOG 2>&1
+  ssh -oStrictHostKeyChecking=no -i ${USERNAME}.key ${USERNAME}@${clamav_ip} "sudo bash -x /home/${USERNAME}/postinstall.bash" >> $DEBUGLOG 2>&1
+  stop_vm ${CLAMAV} >> $DEBUGLOG 2>&1
+  virsh dumpxml ${CLAMAV} > ${CLAMAV}.xml
+  compress_vm ${CLAMAV}
+fi
+[ ! $(vm_exists ${CLAMAV}) = '0' ] && die "can not create ${CLAMAV}"
+start_vm ${CLAMAV} >> $DEBUGLOG 2>&1
+clamav_ip=$(getip_vm ${CLAMAV})
+ssh -oStrictHostKeyChecking=no -i ${USERNAME}.key ${USERNAME}@${clamav_ip} "netstat -ntple" >> $DEBUGLOG 2>&1
+clamav22=$(curl -s ${clamav_ip}:3310 | wc -l)
+if [ $clamav22 -ne 69 ]; then
+   log "WARNING CLAMAV ${CLAMAV} ${clamav_ip} did not replied as expected"
+else
+  log "CLAMAV ${CLAMAV} ${clamav_ip} seems ok"
 fi
