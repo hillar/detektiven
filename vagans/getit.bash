6e678808d21c72929ff15536a88d557bd00ab880
HILLAR AARELAID <hillar.aarelaid@politsei.ee>
2018-04-18 17:35:13 +0300

    check previous download

--- vagans/getit.bash
+++ vagans/getit.bash
@@ -7,20 +7,27 @@ log() { echo "$(date) $0: $*"; }
 die() { log ": $*" >&2; exit 1; }
 
 TARGETDIR=$(pwd)
-log "downloading detektiven"
-cd /tmp
-[ -f master.tar.gz ] && rm master.tar.gz
+# check if we are running from detektiven/scripts
+# if yes, die
+tmp1=$(basename ${TARGETDIR})
+tmp2=$(basename $(dirname ${TARGETDIR}))
+[ "${tmp1}" = "scripts" -a "${tmp2}" = "detektiven" ] && die "already copied here ${TARGETDIR}"
+# check if exits already
+[ -f ${TARGETDIR}/detektiven/scripts/getup.bash ] && die "already copied to ${TARGETDIR}/detektiven/scripts"
+TMP=$(mktemp -d)
+cd ${TMP}
 wget -q https://github.com/hillar/detektiven/archive/master.tar.gz
 [ -f master.tar.gz ] || die 'failed to get detektiven source from https://github.com/hillar/detektiven'
 log "unpacking detektiven"
 tar -xzf master.tar.gz
 [ -f detektiven-master/vagans/getup.bash ] || die 'ERROR - no getup.bash'
-mkdir -p ${TARGETDIR}/detektiven/scripts
-[ -d ${TARGETDIR}/detektiven/scripts ] || die "can not create directory ${TARGETDIR}/detektiven/scripts"
-cp -r detektiven-master/vagans/* ${TARGETDIR}/detektiven/scripts
+mkdir -p ${TARGETDIR}/detektiven
+[ -d ${TARGETDIR}/detektiven ] || die "can not create directory ${TARGETDIR}/detektiven"
+mv detektiven-master/vagans detektiven-master/scripts
+cp -r detektiven-master/scripts ${TARGETDIR}/detektiven
 [ $? -ne 0 ] && die "failed to copy scripts to ${TARGETDIR}/detektiven/scripts"
-rm master.tar.gz
-#rm -rf detektiven-master
+rm -rf ${TMP}
+rm ${TARGETDIR}/detektiven/scripts/getit.bash
 log "build scripts copied to ${TARGETDIR}/detektiven/scripts"
 log "read ${TARGETDIR}/detektiven/scripts/getup.bash before you run it!"
 cd ${TARGETDIR}/detektiven/scripts/
